7f16f6964e73c9f9bc719e4f0c8ce7296f1a6264
DESKTOP-OF9VJ45\sanje <sanjeevthapagaha@gmail.com>
2023-04-06 19:54:16 +0545

    created simple

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//
+//create a simple http server
+
+//importing http
+const http = require('http');
+//describing port and host
+const hostName = 'localhost'
+const port  = 4000
+
+//creating a simple server with header and end
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<h1>This is a simple header</h1>')
+})
+
+
+//starting a server at port 4000 of localhost
+server.listen(port,hostName,()=>{
+    console.log(`server has started at http://${hostName}:${port}`)
+})
